25803fa05635b2326fb784411b648f14ca8aed71
Rafael Souza Barbosa <rafaelsouz10@gmail.com>
2025-02-01 21:11:18 -0300

    correções pin leds para BitDogLab

--- include/config_leds.h
+++ include/config_leds.h
@@ -1,9 +1,10 @@
 #ifndef CONFIG_LEDS_H
 #define CONFIG_LEDS_H
 
-const uint LED_RED = 6;    // Define o pino GPIO 11 para controlar o LED vermelho.
-const uint LED_YELLOW = 7;     // Pino GPIO 12 para o LED amarelo.
-const uint LED_GREEN = 8;   // Pino GPIO 13 para o LED verde.
+#define LED_RED 13    // Define o pino GPIO 13 para controlar o LED vermelho.
+#define LED_GREEN 12   // Pino GPIO 12 para o LED verde.
+#define LED_YELLOW_BLUE 11     // Pino GPIO 11 para o LED amarelo no Wokwi e LED verde na BitDogLab.
+
 
 // Variáveis globais
 bool leds_active  = false;  // Indica se o temporizador está em execução.
@@ -12,17 +13,17 @@ int state = 0; // Variável para controlar o estado da sequência de LEDs.
 void setup_gpio_leds() {
     gpio_init(LED_RED);
     gpio_set_dir(LED_RED, GPIO_OUT);
-    gpio_init(LED_YELLOW);
-    gpio_set_dir(LED_YELLOW, GPIO_OUT);
+    gpio_init(LED_YELLOW_BLUE);
+    gpio_set_dir(LED_YELLOW_BLUE, GPIO_OUT);
     gpio_init(LED_GREEN);
     gpio_set_dir(LED_GREEN, GPIO_OUT);
 }
 
 // Função para ligar todos os LEDs
 void turn_on_all_leds() {
-    gpio_put(LED_YELLOW, true);
     gpio_put(LED_RED, true);
     gpio_put(LED_GREEN, true);
+    gpio_put(LED_YELLOW_BLUE, true);
 }
 
 // Função de callback para desligar o LED após o tempo programado.
@@ -33,7 +34,7 @@ int64_t turn_off_callback(alarm_id_t id, void *user_data) {
             state = 1;
         break;
         case 1:
-            gpio_put(LED_YELLOW, false);
+            gpio_put(LED_YELLOW_BLUE, false);
             state = 2;
         break;
         case 2:
